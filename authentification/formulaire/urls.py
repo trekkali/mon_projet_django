c2b25a98fa8e17fa3f8c999d8b92b2239fd11773
BeugreAndysemariel <andysemariel@gmail.com>
2024-05-14 14:31:52 +0000

    actualisation

--- authentification/formulaire/urls.py
+++ authentification/formulaire/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from .views import signup, user_login, dashboard, reset
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    
     path('signup/', signup, name='signup'),
     path('login/', user_login, name='login'),  # Mettez à jour cette ligne
     path('dashboard/', dashboard, name='dashboard'),
     path('reset/', reset, name='reset'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
--- authentification/formulaire/views.py
+++ authentification/formulaire/views.py
@@ -5,8 +5,11 @@ from django import forms
 from django.contrib import messages  # Ajout de l'import pour les messages d'erreur
 from .forms import SignUpForm  # Importez votre formulaire SignUpForm
 from .forms import LoginForm
+from django.contrib.auth import logout
 
 def signup(request):
+    email_exists = False  # Initialiser la variable à False
+
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
@@ -17,8 +20,9 @@ def signup(request):
             subscribe_to_newsletter = form.cleaned_data['subscribe_to_newsletter']
             user = User.objects.filter(username=email).first()
             if user is not None:
-                messages.error(request, "User with this email already exists.")
-                return render(request, 'signup.html', {'form': form})
+                email_exists = True  # Mettre à True si l'e-mail existe déjà
+                # messages.error(request, "User with this email already exists.")
+                # return render(request, 'signup.html', {'form': form})
             else:
                 user = User.objects.create_user(username=email, email=email, password=password, first_name=first_name, last_name=last_name)
                 # Authentifier et connecter l'utilisateur nouvellement créé
@@ -32,9 +36,13 @@ def signup(request):
             return render(request, 'signup.html', {'form': form})
     else:
         form = SignUpForm()
-    return render(request, 'signup.html', {'form': form})
+    return render(request, 'signup.html', {'form': form, 'email_exists': email_exists})
 
 
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')  # Redirige vers la page de connexion
 def user_login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
